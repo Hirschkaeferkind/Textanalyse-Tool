247cc67ea9a961e80f0e3589b26d3a5c53645b2d
Hirschkaeferkind <142298394+Hirschkaeferkind@users.noreply.github.com>
2025-05-08 11:18:34 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ from setuptools import setup
 APP = ['Parser_v2.py']
 OPTIONS = {
     'argv_emulation': True,
-    'frameworks': [
-        '/opt/homebrew/opt/tcl-tk/lib/libtk8.6.dylib',
-        '/opt/homebrew/opt/tcl-tk/lib/libtcl8.6.dylib',
-    ],
+    'packages': [],
+    # Entferne "frameworks"
 }
-
 
 setup(
     app=APP,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
+
